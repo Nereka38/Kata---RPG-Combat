31de4f882cd9a3dfb3fa8b68a6dda29512cdd4cc
Nerea <fernandezcuestanerea@gmail.com>
2022-07-27 09:03:23 +0200

    add kata

--- src/Character.php
+++ src/Character.php
@@ -4,8 +4,51 @@ namespace App;
 
 class Character {
 
+    private int $health;
+    private int $level;
+    private bool $live;
+    private int $damage;
     
+
+    public function __construct() 
+    {
+
+        $this->health = 1000;
+        $this->level = 1;
+        $this->live = $this->health;
+        $this->damage = 500;
+
+    }
+
+    public function getHealth() 
+    {
+        return $this->health;
+    }
+
+    public function setHealth($health) 
+    {
+        $this->health = $health;
+    }
     
+    public function getLevel() 
+    {
+        return $this->level;
+    }
+
+    public function isALive() 
+    {
+       return $this->Live;
+    }
+
+    public function isDead() 
+    {
+        return $this->health == 0;
+    }
+
+    public function attack($enemy, $attackPoints) {
+        
+    }
+
 }
 
 ?>
--- tests/CharacterTest.php
+++ tests/CharacterTest.php
@@ -9,12 +9,70 @@ use App\Faction;
 class CharacterTest extends TestCase {
 
 	/** @test */
-	public function test_function_example()
+	public function test_character_Health_starting_at_1000_points()
 	{
-		$this->assertEquals(4, 4);
+		//Given
+		$goku = new Character;
+
+		//When
+		$gokuHealth = $goku->getHealth();
+
+		//Then
+		$this->assertEquals(1000, $gokuHealth);
+
 	}
 	
+	public function test_character_Level_starting_at_1()
+	{
+		//Given
+		$goku = new Character;
 
+		//When
+		$gokuLevel = $goku->getLevel();
+
+		//Then
+		$this->assertEquals(1, $gokuLevel);
+
+	}
+
+	public function test_may_be_Alive_or_Dead()
+	{
+		//Given
+		$goku = new Character;
+
+		//When
+		$gokuLive = $goku->getLive();
+
+		//Then
+		$this->assertEquals("True", $gokuLive);
+
+	}
+
+	public function  test_character_Damage()
+	{
+		//Given
+		$goku = new Character;
+
+		//When
+		$gokuDamage = $goku->getDamage();
+
+		//Then
+		$this->assertEquals("False", $gokuDamage);
+
+	}
+
+	public function  test_character_Healing()
+	{
+		//Given
+		$goku = new Character;
+
+		//When
+		$gokuHealing = $goku->getHealing();
+
+		//Then
+		$this->assertEquals("True", $gokuHealing);
+
+	}
 }
 
 
